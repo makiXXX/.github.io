fbe205f5daa09538327285fb36e3ab36aa6a15eb
makiXXX <42858805+makiXXX@users.noreply.github.com>
2019-03-01 17:59:30 +0900

    Add files via upload

---
+++ precache-manifest.06f1f8d3bca8a3d686605b3d5bdbb652.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec530755a9cebfa8f9f60fbcb90da848",
+    "url": "/img/apple-touch-icon.png"
+  },
+  {
+    "revision": "fad72cb8cb495d5b01d3fa9fe193431c",
+    "url": "/sound/voice6.mp3"
+  },
+  {
+    "revision": "132460b02802c4e0d6ea3ac3f6d2681e",
+    "url": "/sound/voice8.mp3"
+  },
+  {
+    "revision": "a9690b4343df31cfddb2273dc555aee1",
+    "url": "/sound/voice9.mp3"
+  },
+  {
+    "revision": "890e2024ac21e178e54e2f86b8c3aee2",
+    "url": "/sound/voice7.mp3"
+  },
+  {
+    "revision": "d81253e6b40c3a67c7e92da7d1629154",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b8e2a9560577d100968",
+    "url": "/js/about.cf3736f9.js"
+  },
+  {
+    "revision": "713d24a23015e529ab44c28a9483ad42",
+    "url": "/sound/voice4.mp3"
+  },
+  {
+    "revision": "c5ebfd564d5098d6c14456e5a650f43a",
+    "url": "/sound/voice11.mp3"
+  },
+  {
+    "revision": "1228d0cf6eefbc0226f08ef31d61db09",
+    "url": "/sound/voice5.mp3"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "4a0d9e1104cf3bb9eb9bde1268e8831d",
+    "url": "/sound/voice10.mp3"
+  },
+  {
+    "revision": "d7005bd63ff179dcf312",
+    "url": "/js/app.50ffe543.js"
+  },
+  {
+    "revision": "490239778878a0fd4edc4d24cf0222b5",
+    "url": "/sound/voice1.mp3"
+  },
+  {
+    "revision": "b031904c459a783a1c6f",
+    "url": "/js/chunk-vendors.4b370723.js"
+  },
+  {
+    "revision": "2a302459eb7be95c93a1046f62da71be",
+    "url": "/sound/voice3.mp3"
+  },
+  {
+    "revision": "c0acd4538d84c1a162035477d1cc7a7d",
+    "url": "/sound/voice2.mp3"
+  },
+  {
+    "revision": "64fbe2d4a965f4bae58d7864b0c620f4",
+    "url": "/css/style.css"
+  },
+  {
+    "revision": "a733802738b64fbe2d50fb8f8d90c525",
+    "url": "/css/normalize.min.css"
+  },
+  {
+    "revision": "0465be1238dcb9726aad7f0429d8af96",
+    "url": "/img/android-chrome-192x192.png"
+  },
+  {
+    "revision": "87198161b9f470b18f7a11817c37ec79",
+    "url": "/img/icon-192.png"
+  },
+  {
+    "revision": "d43fc2790a7f80fa505d5ea7ba44a8cd",
+    "url": "/img/moni_figure.png"
+  },
+  {
+    "revision": "0f0d3105b5d60c53ceb1e942f4652a1e",
+    "url": "/img/mstile-150x150.png"
+  },
+  {
+    "revision": "bfb916355eb3cf7aa20fbdf436b07d1c",
+    "url": "/img/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "33925c5855963171544dfc9869d07f61",
+    "url": "/img/browserconfig.xml"
+  },
+  {
+    "revision": "edd4d1c7353bdb892afed0133215311e",
+    "url": "/img/android-chrome-256x256.png"
+  },
+  {
+    "revision": "31359b994c3995c42da6609b0eac1921",
+    "url": "/img/icon-256.png"
+  },
+  {
+    "revision": "3cf8646f6e466c603fbe001988a643c5",
+    "url": "/img/site.webmanifest"
+  },
+  {
+    "revision": "5dc0a4b61899f846da29effa08f05cc5",
+    "url": "/img/favicon-32x32.png"
+  },
+  {
+    "revision": "83ffa2178312bd9f46553aca2d699696",
+    "url": "/img/favicon-16x16.png"
+  },
+  {
+    "revision": "d7005bd63ff179dcf312",
+    "url": "/css/app.7e7cfee3.css"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,29 @@
-self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
-});
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
 
-self.addEventListener('activate', function(e) {
-  console.log('[ServiceWorker] Activate');
-});
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
-self.addEventListener('fetch', function(event) {});
+importScripts(
+  "/precache-manifest.06f1f8d3bca8a3d686605b3d5bdbb652.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "my-tst-app"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
